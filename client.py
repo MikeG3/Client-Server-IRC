c713ef1c4a4424ebc07f93c4b5fea659b1430014
Mike <39208141+MikeG3@users.noreply.github.com>
2021-03-04 13:31:13 -0500

    Base code for server echo
    original code commented out

--- client.py
+++ client.py
@@ -1,20 +1,37 @@
 # Echo client program
-# Copied from https://www.youtube.com/watch?v=Lbfe3-v7yE0
+"""
+CLIENT DESCRIPTION
+Client constructs socket and binds it to its IP ADDRESS and PORT
+Client inputs userName for IRC (NICK)
+Client can join and disconnect to IRC Server
+Client receives all broadcasted messages from IRC Server
+Client can send message to IRC Server
+"""
 
 import socket
-
+HOST = socket.gethostname()
 PORT = 4200
 
 # Get username for IRC
 print("Please enter the user name for this client")
 userName = input()
 
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
+print('Received', repr(data))
+
+
+"""
 # Create socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+a = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Connect to server, with host-IP at Port 4200
-sock.connect((socket.gethostname(), PORT))
+a.connect((socket.gethostname(), PORT))
 
 # Receive and print message (max size)
-message = sock.recv(512)
+message = a.recv(512)
 print(message.decode("utf-8"))
+"""
 
