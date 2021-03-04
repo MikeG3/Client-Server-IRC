4af61955d5999d740b17dd9122dd56f89272f2fc
Mike <39208141+MikeG3@users.noreply.github.com>
2021-03-04 13:31:41 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,18 +1,35 @@
 # Echo server program
-# Copied https://www.youtube.com/watch?v=Lbfe3-v7yE0
+"""
+SERVER DESCRIPTION
+Server will construcr a socket and bind its IP ADDRESS and PORT to it
+Server listens to responses at the socket
+Server will allow clients to join its general channel
+    Nickname required to enter and allow for message transfer
+Server Accepts and broadcasts messages
+Server handles disconnections from clients
+"""
 
 import socket
 
-HOST = "Server X"
+HOST = ""
 PORT = 4200
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind((socket.gethostname(), PORT))
-sock.listen(12)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(12)
 
+conn, address = s.accept()
+with conn:
+    print('Connected by', address)
+    while True:
+        data = conn.recv(1024)
+        if not data: break
+        conn.sendall(data)
+
+"""
 while True:
     clientsocket: socket
-    clientsocket, address = sock.accept()
+    clientsocket, address = s.accept()
     print(f"Connected to {socket.gethostname()}")
     clientsocket.send(bytes(f"Server response: You are now connected to {HOST}", "utf-8"))
-
+"""
