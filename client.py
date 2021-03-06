a6421c03d9b39fda0b050e4b083a7f0f12ec876a
Mike <39208141+MikeG3@users.noreply.github.com>
2021-03-06 02:55:08 -0500

    Update client.py

--- client.py
+++ client.py
@@ -17,29 +17,39 @@ PORT = 4200
 # Get username for IRC
 print("Please enter the user name for this client")
 userName = input()
-
 print(f"\nyour userName is {userName}")
 
 # CREATE SOCKET AND CONNECT IT TO THE IRC SERVER
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-print(f"\nA socket has been created and connected to the IRC Server")
 
 
+# READ MESSAGES
 def receive_message():
     while True:
+        # READ MESSAGE FROM SOCKET
         message = s.recv(5000).decode("ascii")
+        # BACKEND NICK HANDLING
         if message == "Please send your nickname for the IRC":
             s.send(userName.encode("ascii"))
-            print(f"\nYour nickname {userName} has been registered with IRC\nYou are now welcome to chat")
+            # Validate nickname or close connection if rejected
+            # print(f"\nYour nickname {userName} has been registered with IRC\nYou are now welcome to chat")
         else:
             print(message)
 
 
+# WRITE MESSAGES
 def send_message():
     while True:
         message = userName + ": " + input()
+        # SEND MESSAGE TO SOCKET
         s.send(message.encode("ascii"))
+
+
+# DISCONNECT FROM SERVER
+def close_connection():
+    # code to disconnect
+    print("Disconnecting from server")
 
 
 # CREATE 2 THREADS, FOR MULTI-TASKING: 1 FOR SENDING MESSAGES AND 1 FOR RECEIVING MESSAGES
@@ -47,6 +57,5 @@ receive_message_thread = threading.Thread(target=receive_message)
 send_message_thread = threading.Thread(target=send_message)
 
 # RUN BOTH THREADS
-print(f"\nNow running both threads {userName}")
 receive_message_thread.start()
 send_message_thread.start()
